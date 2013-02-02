1831c2740fe391ae6dd537820f7e00b8f41f63f7
Daniel Auener <daniel.auener@gmail.com>
2013-02-02 21:28:12 +0100

    minor text changes

--- loop-empty.php
+++ loop-empty.php
@@ -3,7 +3,7 @@
 	<h1 class="post-title"><?php _e( 'Nothing Found', 'northeme' ); ?></h1>
 
 	<div class="post-content">
-		<p><?php _e( 'Apologies, but no results were found. Perhaps searching will help find a related post.', 'northeme' ); ?></p>
+		<p><?php _e( 'No results found. Find another post:', 'northeme' ); ?></p>
 		<?php get_search_form(); ?>
 	</div>
 
